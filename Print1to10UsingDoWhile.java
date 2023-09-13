7569cf8154dab3a52043c1b677b6fbfe4ddefa24
VISHNU2105204 <120297452+VISHNU2105204@users.noreply.github.com>
2023-09-13 14:25:43 +0530

    Create Print1to10UsingDoWhile.java

---
+++ Print1to10UsingDoWhile.java
@@ -0,0 +1,14 @@
+// WAJP to print numbers from 1 to 10 using do while loop.
+
+public class Print1to10UsingDoWhile{
+  public static void main(String[] args){
+    
+    int i=1;
+    do
+    {
+      System.out.println(i);
+      i++;
+    }
+    while(i<=10);
+  }
+}
