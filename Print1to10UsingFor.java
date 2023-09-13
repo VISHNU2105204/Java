57bdc5e24ae16f859a04d842a9d611026d59682d
VISHNU2105204 <120297452+VISHNU2105204@users.noreply.github.com>
2023-09-13 14:34:24 +0530

    Create Print1to10UsingFor.java
    none

---
+++ Print1to10UsingFor.java
@@ -0,0 +1,13 @@
+// WAJP to print numbers from 1 to 10 using For loop.
+
+public class Print1to10UsingFor {
+
+	public static void main(String[] args) {
+	    for(int i=1;i<=10;i++)
+	    {
+	    	System.out.println(i);
+	    }
+
+	}
+
+}
