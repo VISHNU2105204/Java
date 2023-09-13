4f1ace4a4444b73dc8d1b900be704d524066296f
VISHNU2105204 <120297452+VISHNU2105204@users.noreply.github.com>
2023-09-13 11:16:37 +0530

    Create EmployeeDetails.java
    none

---
+++ EmployeeDetails.java
@@ -0,0 +1,34 @@
+/*WAJP to read the data from the user to provide the Student Name,
+Student Enrolment nu, Add, Phone Nu, Division.*/ 
+
+import java.util.Scanner;
+
+public class EmployeeDetails {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Employee Name: ");
+        String employeeName = sc.nextLine();
+
+        System.out.print("Enter Employee ID: ");
+        int employeeId = sc.nextInt();
+        sc.nextLine(); 
+
+        System.out.print("Enter Salary: ");
+        double salary = sc.nextDouble();
+        sc.nextLine(); 
+
+        System.out.print("Enter Department Name: ");
+        String departmentName = sc.nextLine();
+
+        System.out.print("Enter Designation Code: ");
+        int designationCode = sc.nextInt();
+
+
+        System.out.println("Employee Name: " + employeeName);
+        System.out.println("Employee ID: " + employeeId);
+        System.out.println("Salary: " + salary);
+        System.out.println("Department Name: " + departmentName);
+        System.out.println("Designation Code: " + designationCode);
+    }
+}
