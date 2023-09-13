f75b8cf5f0af662721a6082b9bae3651de1cf93a
VISHNU2105204 <120297452+VISHNU2105204@users.noreply.github.com>
2023-09-13 13:02:02 +0530

    Update DaysInMonth.java

--- DaysInMonth.java
+++ DaysInMonth.java
@@ -17,7 +17,8 @@ public class DaysInMonth {
 
         int daysInMonth;
 
-        switch (month) {
+        switch (month) 
+        {
             case 1: case 3: case 5: case 7: case 8: case 10: case 12:
                 daysInMonth = 31;
                 break;
@@ -25,7 +26,8 @@ public class DaysInMonth {
                 daysInMonth = 30;
                 break;
             case 2:
-                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) 
+                {
                     daysInMonth = 29; 
                 } else {
                     daysInMonth = 28; 
